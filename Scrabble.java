6873098e1101532e0c02c5c8dc61b24b4ab49a8a
unknown <boaz6151@gmail.com>
2024-12-07 21:41:20 +0200

    Comp

--- Scrabble.java
+++ Scrabble.java
@@ -1,25 +1,25 @@
 public class Scrabble {
 
     static final String WORDS_FILE = "dictionary.txt";
-    static final int[] SCRABBLE_LETTER_VALUES = { 1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10 };
+    static final int[] SCRABBLE_LETTER_VALUES = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
     static int HAND_SIZE = 10;
     static int MAX_NUMBER_OF_WORDS = 100000;
     static String[] DICTIONARY = new String[MAX_NUMBER_OF_WORDS];
     static int NUM_OF_WORDS;
 
     public static void init() {
-        In in = new In(WORDS_FILE);
+        In fileReader = new In(WORDS_FILE);
         System.out.println("Loading word list from file...");
         NUM_OF_WORDS = 0;
-        while (!in.isEmpty()) {
-            DICTIONARY[NUM_OF_WORDS++] = in.readString().toLowerCase();
+        while (!fileReader.isEmpty()) {
+            DICTIONARY[NUM_OF_WORDS++] = fileReader.readString().toLowerCase();
         }
         System.out.println(NUM_OF_WORDS + " words loaded.");
     }
 
     public static boolean isWordInDictionary(String word) {
-        for (int i = 0; i < DICTIONARY.length; i++) {
-            if (DICTIONARY[i] != null && DICTIONARY[i].equals(word)) {
+        for (int i = 0; i < NUM_OF_WORDS; i++) {
+            if (DICTIONARY[i].equals(word)) {
                 return true;
             }
         }
@@ -28,13 +28,21 @@ public class Scrabble {
 
     public static int wordScore(String word) {
         int score = 0;
-        int letterScore = 0;
+        int totalScore = 0;
         for (int i = 0; i < word.length(); i++) {
-            letterScore += SCRABBLE_LETTER_VALUES[word.charAt(i) - 'a'];
+            int letterIndex = word.charAt(i) - 'a';
+            totalScore += SCRABBLE_LETTER_VALUES[letterIndex];
         }
-        score += letterScore * word.length();
-        if (HAND_SIZE == word.length()) score += 50;
-        if (word.indexOf('r') >= 0 && word.indexOf('u') >= 0 && word.indexOf('n') >= 0 && word.indexOf('i') >= 0) score += 1000;
+        score = totalScore * word.length();
+
+        if (HAND_SIZE == word.length()) {
+            score += 50;
+        }
+
+        if (MyString.countChar(word, 'r') > 0 && MyString.countChar(word, 'u') > 0 &&
+            MyString.countChar(word, 'n') > 0 && MyString.countChar(word, 'i') > 0) {
+            score += 1000;
+        }
         return score;
     }
 
@@ -46,54 +54,49 @@ public class Scrabble {
     }
 
     public static void playHand(String hand) {
-		int score = 0;
-		In in = new In();
-		while (hand.length() > 0) {
-			System.out.println("Current Hand: " + MyString.spacedString(hand));
-			System.out.println("Enter a word, or '.' to finish playing this hand:");
-			String input = in.readString().toLowerCase();
-	
-			if (input.equals(".")) break;
-	
-			if (MyString.subsetOf(input, hand)) {
-				if (isWordInDictionary(input) && !input.isEmpty()) {
-					hand = MyString.remove(hand, input);
-					int wordScore = wordScore(input);
-					score += wordScore;
-					System.out.println(input + " earned " + wordScore + " points. Score: " + score + " points\n");
-	
-					// Break out after a valid word
-					if (hand.length() == 0) {
-						System.out.println("Ran out of letters. Total score: " + score + " points");
-						break;
-					}
-				} else {
-					System.out.println("No such word in the dictionary. Try again.\n");
-				}
-			} else {
-				System.out.println("Invalid word. Try again.\n");
-			}
-		}
-	
-		if (hand.length() != 0) {
-			System.out.println("End of hand. Total score: " + score + " points");
-		}
-	}
-	
+        int score = 0;
+        In userInputReader = new In();
+        while (hand.length() > 0) {
+            System.out.println("Current Hand: " + MyString.spacedString(hand));
+            System.out.println("Enter a word, or '.' to finish:");
+            String userInput = userInputReader.readString();
+
+            if (userInput.equals(".")) {
+                break;
+            }
+
+            if (MyString.subsetOf(userInput, hand)) {
+                if (isWordInDictionary(userInput)) {
+                    hand = MyString.remove(hand, userInput);
+                    score += wordScore(userInput);
+                    System.out.println(userInput + " earned " + wordScore(userInput) + " points. Total score: " + score);
+                    System.out.println();
+                } else {
+                    System.out.println("Word not found in dictionary. Try again.");
+                    System.out.println();
+                }
+            } else {
+                System.out.println("Invalid word. Try again.");
+            }
+        }
+        if (hand.length() == 0) {
+            System.out.println("Out of letters. Total score: " + score + " points");
+        } else {
+            System.out.println("End of hand. Total score: " + score + " points");
+        }
+    }
 
     public static void playGame() {
         init();
-        In in = new In();
-
+        In userInputReader = new In();
         while (true) {
-            System.out.println("Enter n to deal a new hand, or e to end the game:");
-            String input = in.readString();
+            System.out.println("Press 'n' for new hand, 'e' to end the game:");
+            String userInput = userInputReader.readString();
 
-            if (input.equals("n")) {
+            if (userInput.equals("n")) {
                 String hand = createHand();
                 playHand(hand);
-            } else if (input.equals("e")) {
-                System.out.println("Thanks for playing!");
+            } else if (userInput.equals("e")) {
                 break;
             } else {
                 System.out.println("Invalid input. Please enter 'n' or 'e'.");
@@ -128,8 +131,8 @@ public class Scrabble {
 
     public static void testPlayHands() {
         init();
-        //playHand("ocostrza");
-        //playHand("arbffip");
-        //playHand("aretiin");
+        playHand("ocostrza");
+        playHand("arbffip");
+        playHand("aretiin");
     }
 }
